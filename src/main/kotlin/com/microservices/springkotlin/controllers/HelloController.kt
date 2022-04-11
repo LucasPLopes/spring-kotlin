3f61af498c9e0145e4a8a49ccfa01d29731d1a4a
Lucas <l.lopes.fga@gmail.com>
2022-04-10 22:16:48 -0300

    add hello, world endpoint

--- src/main/kotlin/com/microservices/springkotlin/controllers/HelloController.kt
+++ src/main/kotlin/com/microservices/springkotlin/controllers/HelloController.kt
@@ -1,12 +1,16 @@
 package com.microservices.springkotlin.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hello")
 class HelloController {
-    @GetMapping
-    fun hello() = "Hello, world"
+    @GetMapping("/{name}")
+    fun hello(@PathVariable name: String): String {
+        if (name.isNotBlank()) return "Hello, $name"
+         return "Hello, World"
+    }
 }
